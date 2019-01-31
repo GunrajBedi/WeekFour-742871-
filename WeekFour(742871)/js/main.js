8c707c1511a3520af6a042a183f15af6dff31b70
visualstudio <visualstudio@230-PC07>
2019-01-31 11:49:00 -0500

    I created a function successfully!!

--- WeekFour(742871)/js/main.js
+++ WeekFour(742871)/js/main.js
@@ -1,1 +1,8 @@
 ﻿// Your code here!
+var date = new Date()
+document.body.innerHTML = "<h1> The Date Today is " + (date.getMonth() + 1) + " / " + date.getDate() + " / " + date.getFullYear() + "</h1>"
+function myFunction() {
+    var date = new Date();
+    document.body.innerHTML = "<h1> The Date Today is " + (date.getMonth() + 1) + " / " + date.getDate() + " / " + date.getFullYear() + "</h1>";
+    alert("Hello");
+}
